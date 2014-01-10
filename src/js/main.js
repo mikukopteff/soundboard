773d577c7c614307f873479ddee5d71034bc41c6
Mikael Kopteff <miku.kopteff@gmail.com>
2014-01-10 18:45:51 +0200

    Add playing of test audio file from dummy buttons

--- src/js/main.js
+++ src/js/main.js
@@ -6,5 +6,27 @@ require.config({
 });
 
 require(['jquery'], function($) {
- alert($);
+
+    var audio = document.createElement('audio');
+    audio.setAttribute('preload', 'auto');
+    audio.autobuffer = true;
+    document.body.appendChild(audio);
+    audio.load();
+    $('#audio-row').on('click', 'a', playAudio)
+
+    function playAudio(event) {
+        event.preventDefault();
+        createSource(event.target.getAttribute('data-audio-url'));
+        audio.play();
+    }
+
+    function createSource(audioUrl) {
+        console.log(audio);
+        if (audio.childNodes.length > 0) audio.removeChild(audio.childNodes[0]);
+        var source = document.createElement('source');
+        source.type = 'audio/wav';
+        source.src = audioUrl;
+        audio.appendChild(source);
+    }
+
 });
