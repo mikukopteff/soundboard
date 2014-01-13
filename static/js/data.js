61eba50a3fe98cc944bc4c93251b766d2634bf7f
Mikael Kopteff <miku.kopteff@gmail.com>
2014-01-13 18:10:05 +0200

    Move the whole shabang on node.js

---
+++ static/js/data.js
@@ -0,0 +1,21 @@
+define([], function() {
+	var order = ['blue', 'green', 'orange', 'red', 'orange', 'red', 'green'];
+	var classOrder = order.concat(['blue', 'green']).concat(order.reverse());
+
+  	return {
+  		getBoardData: function (callback) {
+			$.ajax('default-board.json').done(function(json) {				
+				console.log(json);
+				for (var i = 0; i < json.boardCells.length; i++) {
+					var newCell = $("#cell").clone();
+					newCell.removeAttr('id');
+					newCell.addClass(classOrder[i]);
+					newCell.children('a').text(json.boardCells[i].text)
+						.attr('href','#').attr('data-audio-url', json.boardCells[i].audioUrl)
+						.click(callback);
+					newCell.appendTo(".board").show();	
+				}
+			});  			
+  		}
+  	}
+});
