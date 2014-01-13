e1ec478f3e157d45186dfdde3d6f7b405a8f115b
Mikael Kopteff <miku.kopteff@gmail.com>
2014-01-13 18:14:59 +0200

    Remove useless cahching form static files

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 
-var oneDay = 86400000;
-
 app.use(express.compress());
 
-app.use(express.static(__dirname + '/static', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/static'));
 
 app.listen(process.env.PORT || 8000);
