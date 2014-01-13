c00fd0ea3b807ce6ca32832c138e4bc548701425
Mikael Kopteff <miku.kopteff@gmail.com>
2014-01-13 10:09:33 +0200

    Refactor css and remove minor css bug in js

--- src/js/data.js
+++ src/js/data.js
@@ -1,13 +1,14 @@
 define([], function() {
-	var order = ['one', 'two', 'three', 'four', 'three', 'four', 'two'];
-	var classOrder = order.concat(['one']).concat(order.reverse()); 
+	var order = ['blue', 'green', 'orange', 'red', 'orange', 'red', 'green'];
+	var classOrder = order.concat(['blue']).concat(order.reverse()); 
+
   	return {
   		getBoardData: function (callback) {
 			$.ajax('default-board.json').done(function(json) {				
 				console.log(json);
 				for (var i = 0; i < json.boardCells.length; i++) {
-					console.log('foo' + i)
 					var newCell = $("#cell").clone();
+					newCell.removeAttr('id');
 					newCell.addClass(classOrder[i]);
 					newCell.children('a').text(json.boardCells[i].text)
 						.attr('href','#').attr('data-audio-url', 'test.wav')
