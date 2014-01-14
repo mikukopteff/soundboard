03fdd19ccac200e94715ec1c9589e836ff3052e8
Mikael Kopteff <miku.kopteff@gmail.com>
2014-01-14 21:24:05 +0200

    Add new sounds to board

--- static/js/data.js
+++ static/js/data.js
@@ -1,6 +1,5 @@
 define([], function() {
-	var order = ['blue', 'green', 'orange', 'red', 'orange', 'red', 'green'];
-	var classOrder = order.concat(['blue', 'green']).concat(order.reverse());
+	var classOrder = ['blue', 'green', 'orange', 'red', 'orange', 'red', 'blue', 'green'];
 
   	return {
   		getBoardData: function (callback) {
@@ -9,7 +8,8 @@ define([], function() {
 				for (var i = 0; i < json.boardCells.length; i++) {
 					var newCell = $("#cell").clone();
 					newCell.removeAttr('id');
-					newCell.addClass(classOrder[i]);
+					var y = classOrder.length > i ? i : i - classOrder.length;
+					newCell.addClass(classOrder[y]) 
 					newCell.children('a').text(json.boardCells[i].text)
 						.attr('href','#').attr('data-audio-url', json.boardCells[i].audioUrl)
 						.click(callback);
