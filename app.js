81e3e927e5e5a3f74cd59e3c5beb6c2c1ab7cb49
Mikael Kopteff <miku.kopteff@gmail.com>
2014-01-26 00:53:33 +0200

    Add server side templating for fb sharing

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var _ = require('underscore')
+cons = require('consolidate')
+var fs = require('fs')
 
-app.use(express.compress());
+var app = express()
+app.use(express.compress())
 
-app.use('/', express.static(__dirname + '/static'));
+app.use('/', express.static(__dirname + '/static'))
 app.use('/test', express.static(__dirname + '/test'))
 app.use('/test/lib/mocha', express.static(__dirname + '/node_modules/mocha'))
 app.use('/test/lib/chai', express.static(__dirname + '/node_modules/chai'))
 app.use('/test/lib/js-fixtures', express.static(__dirname + '/node_modules/js-fixtures'))
 //app.use('/test/chai-jquery', express.static('node_modules/chai-jquery'))
 
-app.listen(process.env.PORT || 8000);
+app.engine('html', cons.underscore)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+
+console.log('Server started')
+app.listen(process.env.PORT || 8000)
+
+app.get('/app', function(req, res){
+	res.render('index', {tagTitle : 'foo', tagName: 'faa'})
+})
+
+
+
+
