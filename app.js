92d0e115dee77e23355ca5ef005c7aaa45feb0cb
Mikael Kopteff <miku.kopteff@gmail.com>
2014-01-26 10:38:41 +0200

    Add fb sharing for whole page

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var _ = require('underscore')
 cons = require('consolidate')
-var fs = require('fs')
 
 var app = express()
 app.use(express.compress())
@@ -11,7 +10,7 @@ app.use('/test', express.static(__dirname + '/test'))
 app.use('/test/lib/mocha', express.static(__dirname + '/node_modules/mocha'))
 app.use('/test/lib/chai', express.static(__dirname + '/node_modules/chai'))
 app.use('/test/lib/js-fixtures', express.static(__dirname + '/node_modules/js-fixtures'))
-//app.use('/test/chai-jquery', express.static('node_modules/chai-jquery'))
+app.use('/test/chai-jquery', express.static('node_modules/chai-jquery'))
 
 app.engine('html', cons.underscore)
 app.set('view engine', 'html')
@@ -21,7 +20,9 @@ console.log('Server started')
 app.listen(process.env.PORT || 8000)
 
 app.get('/app', function(req, res){
-	res.render('index', {tagTitle : 'foo', tagName: 'faa'})
+	res.render('index', {title : 'The most Epic of Soundboards', 
+		url: req.protocol + "://" + req.get('host') + req.url,
+		description: 'Awesome Soundboard app'})
 })
 
 
