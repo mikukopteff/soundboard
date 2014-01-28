b308ca8ae887eb4c20cef4647e185df9938585c9
Mikael Kopteff <miku.kopteff@gmail.com>
2014-01-28 09:37:29 +0200

    Cleanup sharing module

--- static/js/sharing.js
+++ static/js/sharing.js
@@ -3,18 +3,17 @@ define([], function() {
   	return {
   		shareButton: function (element, board) {
       var id =  $(element).parent().parent().attr('id')
-      console.log(board.boardCells[id].text)
 			$(element).click(function(e){
             	e.preventDefault(); 
             	FB.ui({
                   app_id: '736748319669744',
                 	method: 'feed',
                   source: board.boardCells[id].audioUrl,
-                	name: board.boardCells[id].text,
+                	name: 'Epic Board sound: ' + board.boardCells[id].text,
                 	link: window.location.href,
                 	picture: "https://s3-eu-west-1.amazonaws.com/epic-board/epic-board.jpg",
                 	caption: window.location.href,
-                	description: 'Listen to this awesome sound!',
+                	description: 'Listen to a sound on Epic Soundboard',
                 	message: ''
             	});     
         	});
