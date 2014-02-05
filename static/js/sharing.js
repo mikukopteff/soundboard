aa51592d57d3a99ed4ddce50353aaebe3c6feec3
Mikael Kopteff <miku.kopteff@gmail.com>
2014-02-05 10:32:39 +0200

    Finish responsive sharing dialog

--- static/js/sharing.js
+++ static/js/sharing.js
@@ -3,19 +3,30 @@ define([], function() {
   	return {
   		shareButton: function (element, board) {
       var id =  $(element).parent().parent().attr('id')
-			$(element).click(function(e){
+      var shareDialog = $(element).parent().parent().children('.share-dialog')
+      var shareUrl = window.location.href.substr(0, window.location.href.lastIndexOf('/') + 1) + id
+
+			$(shareDialog).children('input').attr('value', shareUrl)
+      
+      $(element).click(function(e){
+        e.preventDefault() 
+        $(shareDialog).toggle(1000)
+        $(shareDialog).children('input').select()
+      });
+
+      $(shareDialog).children('a.share-item').click(function(e){
             	e.preventDefault() 
             	FB.ui({
                   app_id: '736748319669744',
                 	method: 'feed',
-                  //source: board.boardCells[id].audioUrl,
                 	name: 'Epic Board sound: ' + board.boardCells[id].text,
-                	link: window.location.href.substr(0, window.location.href.lastIndexOf('/') + 1) + id,
+                	link: shareUrl,
                 	picture: "https://s3-eu-west-1.amazonaws.com/epic-board/epic-board.jpg",
                 	caption: window.location.href,
                 	description: 'Listen to a sound on Epic Soundboard',
                 	message: ''
-            	});     
+            	});
+              $(shareDialog).hide(1000)     
         	});
   		},
       highlightSelected: function (soundId){
