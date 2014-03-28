82dd09da5508002fa1589098b86e8ff95a45c6b6
Mikael Kopteff <miku.kopteff@gmail.com>
2014-03-28 08:47:11 +0200

    Refactor authentication to use constants

--- auth.js
+++ auth.js
@@ -5,25 +5,28 @@
 var crypto = require('crypto')
 var CipherPassword = 'vNYamdOlq9OLf+OVEVzeZO0WGL29LEL+5KN/aA5hN/A='
 var Key = 'iemFHj9q47DDDJTtHrqAXB96Z/UdlGYR9Z+KxpI0OO4='
+var OutputEncoding = 'base64'
+var InputEncoding = 'utf8'
+var EncryptionAlgo = 'aes128'
 
-var Expiration = 604800000 //week
+var Expiration = 60 * 60 * 24 * 7 * 1000
 var Sep = '|'
 
 function digest(string) {
 	var digester = crypto.createHmac('sha1', 's994Mf54PEgXwgx/2f+Kt5Nbkq6HhgQCYsNDhtvbfC8=')
-	return digester.update(string).digest('utf8')	
+	return digester.update(string).digest(InputEncoding)	
 }
 
 function cipher(string) {
-	var aesCipher = crypto.createCipher("aes128", CipherPassword)
-	var encryptedFirst =  aesCipher.update(string, 'utf8', 'base64')
-	return encryptedFirst + aesCipher.final('base64')
+	var aesCipher = crypto.createCipher(EncryptionAlgo, CipherPassword)
+	var encryptedFirst =  aesCipher.update(string, InputEncoding, OutputEncoding)
+	return encryptedFirst + aesCipher.final(OutputEncoding)
 }
 
 function decipher(string) {
-	var aesDecipher = crypto.createDecipher("aes128", CipherPassword)
-	var decryptedFirst = aesDecipher.update(string, 'base64', 'utf8')
-	return decryptedFirst + aesDecipher.final('utf8')
+	var aesDecipher = crypto.createDecipher(EncryptionAlgo, CipherPassword)
+	var decryptedFirst = aesDecipher.update(string, OutputEncoding, InputEncoding)
+	return decryptedFirst + aesDecipher.final(InputEncoding)
 }
 
 exports.createToken = function(userId) {
@@ -34,8 +37,8 @@ exports.createToken = function(userId) {
 
 exports.validateToken = function(token) {
 	var decryptedToken = decipher(token)
-	var tokens =  decryptedToken.split('|')
-	var originalHmac  = decryptedToken.substring(decryptedToken.split('|', 2).join('|').length + 1)
+	var tokens =  decryptedToken.split(Sep)
+	var originalHmac  = decryptedToken.substring(decryptedToken.split(Sep, 2).join(Sep).length + 1)
 	var newHmac = digest((tokens[0] + Sep + tokens[1] + Key))
 	if (newHmac == originalHmac && tokens[1] > new Date().getTime()) {
 		return tokens[0]
